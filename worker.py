0bfce4f484cb8d0315ce84e064a845455fa90845
brandonrkp <brandonrkp@gmail.com>
2018-03-16 17:44:34 +0100

    sel
    sel

--- worker.py
+++ worker.py
@@ -55,7 +55,7 @@ async def on_message(message):
          buscar = ' '.join(args)
          print("esto es el args en buscar todo string"+buscar)
          cur=conn.cursor()
-         sql = """SELECT url FROM giftable where tag like %s order by random() limit 1;""",('%buscar%')
+         sql = """SELECT url FROM giftable where tag like '%%(buscar)s%' order by random() limit 1;"""
          #print("esto es el select: "+sql)
          cur.execute(sql)
          rows = cur.fetchall()
