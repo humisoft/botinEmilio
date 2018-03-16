e07707df7f7b71cbf86ab0d5c0289ded39772c58
brandonrkp <brandonrkp@gmail.com>
2018-03-16 13:17:10 +0100

    b
    bd

--- bd.py
+++ bd.py
@@ -1,8 +1,6 @@
 import os
-from urllib import parse
 import psycopg2
 
-parse.uses_netloc.append("postgres")
 DATABASE_URL = os.environ['DATABASE_URL']
 
 conn = psycopg2.connect(DATABASE_URL, sslmode='require')
