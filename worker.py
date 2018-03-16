8c086a02bdf801f6fc5af08d9443a23068b3973e
brandonrkp <brandonrkp@gmail.com>
2018-03-16 19:23:28 +0100

    sel
    sel

--- worker.py
+++ worker.py
@@ -66,7 +66,7 @@ async def on_message(message):
          for row in rows:
             #await client.send_message(message.channel, row[0])
             msg = await client.send_message(message.channel, row[0])
-            await client.add_reaction(msg, "https://discordapp.com/assets/eed2347ee50b0549d498ddf399a48228.svg")
+            await client.add_reaction(msg, "::arrow_left::")
             #msg = await client.send_message(message.channel, 'React with thumbs up or thumbs down.')
             res = await client.wait_for_reaction(['👍', '👎'], message=msg)
             await client.send_message(message.channel, '{0.user} reacted with {0.reaction.emoji}!'.format(res))
