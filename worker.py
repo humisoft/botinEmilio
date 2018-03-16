ba18f62e7d012c6b1460f04359b3571b3bd680a6
brandonrkp <brandonrkp@gmail.com>
2018-03-16 16:53:42 +0100

    sel
    sel

--- worker.py
+++ worker.py
@@ -49,9 +49,9 @@ async def on_message(message):
         if message.content.startswith('t!botin'):
          args = message.content.split(" ")
          del args[0]
-         buscar = ''.join(args)
+         buscar = ' '.join(args)
          cur=conn.cursor()
-         sql = """SELECT url FROM giftable where tag like '%marquez%' order by random() limit 1;"""
+         sql = """SELECT url FROM giftable where tag like '%buscar%' order by random() limit 1;"""
          cur.execute(sql)
          rows = cur.fetchall()
          for row in rows:
