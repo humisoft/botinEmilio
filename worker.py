6d49e7e1da94912d1d00998dda3037fbd628e25c
brandonrkp <brandonrkp@gmail.com>
2018-03-16 16:43:03 +0100

    sel
    sel

--- worker.py
+++ worker.py
@@ -47,10 +47,10 @@ async def on_message(message):
 
         conn = psycopg2.connect(DATABASE_URL, sslmode='require')
         if message.content.startswith('t!botin'):
-         args = message.content.split(" ")
+         #args = message.content.split(" ")
          #del args[0]
          cur=conn.cursor()
-         sql = """SELECT url FROM giftable where tag like '%%(args)s%' order by random() limit 1;"""
+         sql = """SELECT url FROM giftable where tag like '%message.content%' order by random() limit 1;"""
          cur.execute(sql)
          rows = cur.fetchall()
          for row in rows:
