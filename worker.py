486c4e1e6c4cfec5f4bf3a88a5ecc85e0569f86b
brandonrkp <brandonrkp@gmail.com>
2018-03-17 08:50:26 +0100

    c
    c

--- worker.py
+++ worker.py
@@ -52,8 +52,6 @@ async def on_message(message):
          msg = await client.send_message(message.channel, rows[0][0])
          await client.add_reaction(msg, '👍')
          rea = client.get_reaction_users('👍', limit=1, after=279395402606706688)
-         emo = client.get_all_emojis();
-         print("fuera hijoputa"+str(emo))
          if rea == '👍':
             rand = randint(0, 2)
             newMsg = rows[0][rand]
