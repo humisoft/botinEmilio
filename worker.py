9daabe7ff420a9df72b3a8fb0667d4b61215c4e8
brandonrkp <brandonrkp@gmail.com>
2018-03-17 07:55:24 +0100

    c
    c

--- worker.py
+++ worker.py
@@ -50,14 +50,13 @@ async def on_message(message):
          cur.execute("""SELECT url FROM giftable where tag like \'%%%s%%\' order by random();""", (AsIs(buscar),))
          rows = cur.fetchall()
          msg = await client.send_message(message.channel, rows[0][0])
-         await client.add_reaction(msg, 'U+27A1')
-         await client.add_reaction(msg, 'U+2B05')
+         await client.add_reaction(msg, '↔')
          #for row in rows:
             #msg = await client.send_message(message.channel, row[0])
             #await client.add_reaction(msg, 'U+27A1')
             #await client.add_reaction(msg, 'U+2B05')
-            #rea = client.get_reaction_users('👍', limit=1, after=279395402606706688)
-            #if rea == '👍':
+            #rea = client.get_reaction_users('↔', limit=1, after=279395402606706688)
+            #if rea == '↔':
             #    edit = await client.edit_message(msg, "editadooo")    
             #else:
             #    edit = await client.edit_message(msg, "noeditado")
