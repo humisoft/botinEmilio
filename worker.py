bdf907c96b84551c021ff571a9b1886289d436b8
iYosuua <iyosdev@gmail.com>
2018-03-19 20:13:36 +0100

    awer

--- worker.py
+++ worker.py
@@ -52,8 +52,6 @@ async def on_message(message):
          msg = await client.send_message(message.channel, rows[0][0])
          rea = await client.add_reaction(msg, '👍')
          rea = await client.wait_for_reaction(['👍'], message=rea)
-         await asyncio.sleep(1)
-         rea = client.get_reaction_users('👍', limit=1, after=279395402606706688)
          await asyncio.sleep(5)
          await client.send_message(message.channel, '{0.user} reacted with {0.reaction.emoji}!'.format(rea))
          rea = '👍'
