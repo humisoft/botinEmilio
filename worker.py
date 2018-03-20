e5e20eb3ab171354266e0f94c6a385c0c04912ee
brandonrkp <brandonrkp@gmail.com>
2018-03-20 12:25:14 +0100

    c
    c

--- worker.py
+++ worker.py
@@ -69,8 +69,8 @@ async def on_message(message):
             # print("no reaccion")
          for row in rows:
             msg = await client.send_message(message.channel, row[0])
-            await client.add_reaction(msg, '👍'
-            await client.add_reaction(msg, '👎'))
+            await client.add_reaction(msg, '👍')
+            await client.add_reaction(msg, '👎')
             rea = client.get_reaction_users('👍', limit=1, after=279395402606706688)
             if rea == '👍':
                edit = await client.edit_message(msg, "editadooo")    
