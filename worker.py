d0eeb9d9ee0d8f2a57267b2719e6435b02e4a06b
brandonrkp <brandonrkp@gmail.com>
2018-03-20 12:39:25 +0100

    c
    c

--- worker.py
+++ worker.py
@@ -73,7 +73,7 @@ async def on_message(message):
             await client.add_reaction(msg, '👎')
             await asyncio.sleep(5)
             rea = client.get_reaction_users('👍', limit=1, after=279395402606706688)
-            print("esto es el rea"+rea)
+            print("esto es el rea"+str(rea))
             if rea.reaction.emoji == '👍':
                edit = await client.edit_message(msg, "editadooo")    
             else:
