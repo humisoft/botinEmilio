67116fa4999f4cc3228795744479909a7f62c429
iYosuua <jesusrepi@gmail.com>
2018-03-20 11:22:45 +0100

    prueba
    prueba

--- worker.py
+++ worker.py
@@ -50,11 +50,11 @@ async def on_message(message):
          cur.execute("""SELECT url FROM giftable where tag like \'%%%s%%\' order by random();""", (AsIs(buscar),))
          rows = cur.fetchall()
          msg = await client.send_message(message.channel, rows[0][0])
-         rea = await client.add_reaction(msg, '👍')
-         rea = await client.wait_for_reaction(['👍'], message=rea)
-         await asyncio.sleep(5)
+         await client.add_reaction(msg, '👍')
+         await client.wait_for_reaction(['👍'], message=rea)
+         #await asyncio.sleep(10)
+         rea = client.get_reaction_users('👍', limit=1, after=279395402606706688)
          await client.send_message(message.channel, '{0.user} reacted with {0.reaction.emoji}!'.format(rea))
-         rea = '👍'
          if rea is '👍':
             print("si reaccion")
             await client.send_message(message.channel, 'si reaccion')
