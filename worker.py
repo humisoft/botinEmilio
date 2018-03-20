02850baee9e17f89fa686a77415237697da693b3
brandonrkp <brandonrkp@gmail.com>
2018-03-20 12:34:35 +0100

    c
    c

--- worker.py
+++ worker.py
@@ -73,7 +73,7 @@ async def on_message(message):
             await client.add_reaction(msg, '👎')
             await asyncio.sleep(5)
             rea = client.get_reaction_users('👍', limit=1, after=279395402606706688)
-            if rea == '👍':
+            if rea = '👍':
                edit = await client.edit_message(msg, "editadooo")    
             else:
                edit = await client.edit_message(msg, "noeditado")
