9cc6ec29f926cd4dd14aa2824b0d8b46fae9d04f
brandonrkp <brandonrkp@gmail.com>
2018-03-28 23:46:00 +0200

    c
    c

--- worker.py
+++ worker.py
@@ -48,7 +48,7 @@ async def on_message(message):
      response = requests.get(url)
      content_type = response.headers['content-type']
      extension = mimetypes.guess_extension(content_type)
-     stri = ':link: ' + infoUrl(buscar,0) + ' \n TAG: ' + infoTag(buscar,0)
+     stri = '-------------------------------------------------------\n:link: ' + infoUrl(buscar,0) + ' \n:pencil: ' + infoTag(buscar,0) + '\n-------------------------'
      msg = await client.send_message(message.channel, str(stri))
      await client.add_reaction(msg, '🔃')
      
