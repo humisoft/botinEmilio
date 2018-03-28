8d70583a27d64c77a4bc3b3291373cf8d9d387b1
brandonrkp <brandonrkp@gmail.com>
2018-03-28 23:06:47 +0200

    c
    c

--- worker.py
+++ worker.py
@@ -46,7 +46,7 @@ async def on_message(message):
      em.set_image(url=infoUrl(buscar,0))
      msg = await client.send_message(message.channel, embed=em)
      await client.add_reaction(msg, '🔃')
-     print(url)
+     url = infoUrl(buscar,0)
      response = requests.get(url)
      content_type = response.headers['content-type']
      extension = mimetypes.guess_extension(content_type)
