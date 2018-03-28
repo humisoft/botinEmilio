478b0cde6640a956175a0cc3b6b23140db8b1ae2
brandonrkp <brandonrkp@gmail.com>
2018-03-29 00:27:11 +0200

    c
    c

--- worker.py
+++ worker.py
@@ -46,7 +46,7 @@ async def on_message(message):
      #em.set_image(url=infoUrl(buscar,0))
      barraArri = '```\n ```'
      barraAbaj = '```\n ```'
-     stri = barraArri + ':link: ' + infoUrl(buscar,0) + ' \n\n:pencil: *' + infoTag(buscar,0) + '*\n' + barraAbaj
+     stri = barraArri + ':regional_indicator_u::regional_indicator_r::regional_indicator_l: ' + infoUrl(buscar,0) + ' \n\n:regional_indicator_t::regional_indicator_a::regional_indicator_g: *' + infoTag(buscar,0) + '*\n' + barraAbaj
      msg = await client.send_message(message.channel, str(stri))
      await client.add_reaction(msg, '🔃')
      while True:
