b5a8d161d61ffe4fa55a40e097b80951a99714da
brandonrkp <brandonrkp@gmail.com>
2018-03-30 15:41:35 +0200

    c
    c

--- worker.py
+++ worker.py
@@ -45,9 +45,7 @@ async def on_message(message):
      #em = discord.Embed(title='Gif', url=infoUrl(buscar,0), description=infoTag(buscar,0), color=0xff0000)
      #em.set_image(url=infoUrl(buscar,0))
      if infoUrl(buscar,0) or infoTag(buscar,0):
-         barraArri = '```\n ```'
-         barraAbaj = '```\n ```'
-         stri = str(barraArri) + '\n:regional_indicator_u::regional_indicator_r::regional_indicator_l: ' + infoUrl(buscar,0) + ' \n:regional_indicator_t::regional_indicator_a::regional_indicator_g: **' + infoTag(buscar,0) + '**\n\n' + str(barraAbaj)
+         stri = ':regional_indicator_u::regional_indicator_r::regional_indicator_l: ' + infoUrl(buscar,0) + ' \n:regional_indicator_t::regional_indicator_a::regional_indicator_g: **' + infoTag(buscar,0) + '**'
          msg = await client.send_message(message.channel, str(stri))
          await client.add_reaction(msg, '🔃')
          while True:
