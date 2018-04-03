b636f77d33d3ebe5afa9d1559e9b7cdb35b95f8b
brandonrkp <brandonrkp@gmail.com>
2018-04-03 18:38:43 +0200

    c
    c

--- worker.py
+++ worker.py
@@ -52,7 +52,7 @@ async def on_message(message):
       #em.set_image(url=infoUrl(buscar,0))
       if infoUrl(buscar,ran) or infoTag(buscar,ran):
        posiArray = ran
-	   strinPosiArray = posiArray + 1
+       strinPosiArray = posiArray + 1
        stri =  infoUrl(buscar,posiArray) + ' \n**' + infoTag(buscar,posiArray) + '** __' +str(strinPosiArray) + '/' + str(cantidad) + '__'
        msg = await client.send_message(message.channel, str(stri))
        await client.add_reaction(msg, '👈')
