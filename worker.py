d349e275767c34252629a01619fcb19e72aae89c
brandonrkp <brandonrkp@gmail.com>
2018-04-04 09:17:06 +0200

    c
    c

--- worker.py
+++ worker.py
@@ -116,10 +116,8 @@ async def on_message(message):
       await client.send_message(message.channel, ':octagonal_sign:ESTE GIF YA ESTA EN LA BASE DE DATOS:octagonal_sign:')
      else:
       meter(url,tags)
-      #print(message.author)
-      #print(messageAuthor)
-      #mes = messageAuthor + ' ha introducido el gif ' + url + ' con los tags ' + tags
-      #await client.send_message(bot_log, 'prueba de log')
+      mes = messageAuthor + ' ha introducido el gif ' + url + ' con los tags ' + tags
+      await client.send_message(430976826727333898, str(mes))
       sleep(5)
       await client.delete_message(message)
     #ACTUALIZAR TAG GIF 
