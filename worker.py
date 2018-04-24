979c430268f869ce7bd4069ac73f6d66daa7fab1
brandonrkp <brandonrkp@gmail.com>
2018-04-24 17:54:38 +0200

    c
    c

--- worker.py
+++ worker.py
@@ -114,6 +114,7 @@ async def on_message(message):
                 
                #REACCION 🗑
                if '{0.reaction.emoji}'.format(res) == '🗑':
+                await client.clear_reactions(msg)
                 await client.delete_message(msg)
 
        #fin While     
