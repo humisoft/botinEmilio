270b6fea1fac49913e4e1114091a2b40427ceb7e
brandonrkp <brandonrkp@gmail.com>
2018-04-25 15:14:28 +0200

    c
    c

--- worker.py
+++ worker.py
@@ -40,7 +40,7 @@ async def on_message(message):
 
     #MOSTRAR GIF CON REACCION
     if message.content.startswith('.gif') or message.content.startswith('.tag'):
-     print(client.user.id)
+     print(message.author.id)
      args = message.content.split(" ")
      del args[0]
      buscar = '%\' and tag like \'%'.join(args)
