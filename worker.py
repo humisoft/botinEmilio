897a27bd43a4cd9f27991697595eb362b7b4faeb
brandonrkp <brandonrkp@gmail.com>
2018-04-30 20:09:50 +0200

    c
    c

--- worker.py
+++ worker.py
@@ -41,7 +41,7 @@ async def on_reaction_add(reaction, user):
     if reaction.emoji == '📌':
      em = discord.Embed(description=me, color=0xff0000)
      #em.set_author(name=reaction.message.author, icon_url=reaction.message.author.avatar_url)
-     embed.add_field(name=reaction.message.author.avatar_url, value=reaction.message.author.mention, inline=False)
+     em.add_field(name=reaction.message.author.avatar_url, value=reaction.message.author.mention, inline=False)
      em.set_footer(text=str(tim))
      chan = discord.utils.get(client.get_all_channels(), server__name=str(nomServ), name=str(nomChan))
      await client.send_message(chan, embed=em)
