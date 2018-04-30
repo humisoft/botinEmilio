94720bbc3b7db930c9aca5160e492a60480285bc
brandonrkp <brandonrkp@gmail.com>
2018-04-30 19:57:14 +0200

    c
    c

--- worker.py
+++ worker.py
@@ -40,7 +40,7 @@ async def on_reaction_add(reaction, user):
     tim = str(user.mention) + ' ' + str(reaction.message.edited_timestamp)
     if reaction.emoji == '📌':
      em = discord.Embed(description=me, color=0xff0000)
-     em.set_author(name=user.name, icon_url=user.avatar_url)
+     em.set_author(name=reaction.message.author, icon_url=reaction.message.author.avatar_url)
      em.set_footer(text=str(tim))
      chan = discord.utils.get(client.get_all_channels(), server__name=str(nomServ), name=str(nomChan))
      await client.send_message(chan, embed=em)
